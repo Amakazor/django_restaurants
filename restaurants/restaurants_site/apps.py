faf55d84dae498d2bcf3690bd601228b87df4f67
amakazor <amakazor@gmail.com>
2022-01-03 17:46:08 +0100

    added models

--- restaurants/restaurants_site/apps.py
+++ restaurants/restaurants_site/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RestaurantsSiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'restaurants_site'
+    verbose_name = 'Restaurants'
--- restaurants/restaurants_site/models.py
+++ restaurants/restaurants_site/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.TextField()
+    slug = models.SlugField()
+    description = models.TextField()
+    google_maps_embed_link = models.TextField()
+    image = models.ImageField()
+    country = models.TextField()
+    city = models.TextField()
+    street_address = models.TextField()
+
+class Review(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    rate = models.PositiveSmallIntegerField()
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
