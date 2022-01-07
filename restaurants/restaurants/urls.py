74e2d67191b47352eb01c78761d64a8db55c952d
amakazor <amakazor@gmail.com>
2022-01-07 16:09:56 +0100

    restaurant site

--- restaurants/restaurants/urls.py
+++ restaurants/restaurants/urls.py
@@ -22,5 +22,8 @@ import restaurants_site.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('restaurant/<str:slug>', restaurants_site.views.restaurant),
     path('', restaurants_site.views.home),
+    path('<str:catchall>', restaurants_site.views.redirect),
+
 ] + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
